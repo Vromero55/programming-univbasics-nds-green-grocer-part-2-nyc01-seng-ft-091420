b2ee5ebeac0f6be1e666438838bdcf317f1261db
Victor Romero <romeroa.victor@yahoo.com>
2020-09-12 19:26:44 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,18 +1,54 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
+cart.each do |item|
+  coupons.each do |citem|
+    if item[:item] == citem[:item]
+      if item[:count] == citem[:num]
+        wit_coup=item.dup
+        wit_coup[:item]="#{item[:item]} W/COUPON"
+        wit_coup[:price]=citem[:cost]/citem[:num]
+        item[:count] = item[:count]-citem[:num]
+        cart<<wit_coup
+      elsif item[:count] > citem[:num]
+        wit_coup=item.dup
+        wit_coup[:item]="#{item[:item]} W/COUPON"
+        wit_coup[:price]=citem[:cost]/citem[:num]
+        item[:count]=item[:count]-citem[:num]
+        wit_coup[:count]=citem[:num]
+        cart<<wit_coup
+      else
+        cart
+
+
+  # Consult README for inputs alearnd outputs
+  #.dup copys w.e u want so u can use it to copy the item hash
   # REMEMBER: This method **should** update cart
 end
+end
+end
+end
+cart
+end
+
 
 def apply_clearance(cart)
+  cart.each do |item|
+    if item[:clearance]==true
+      item[:price]=item[:price]-(item[:price] * 0.20)
+    end
+  end
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
 end
 
 def checkout(cart, coupons)
+  newcart=consolidate_cart(cart)
+  binding.pry
+  apply_coupons(newcart, coupons)
+  apply_clearance(newcart)
   # Consult README for inputs and outputs
   #
   # This method should call
